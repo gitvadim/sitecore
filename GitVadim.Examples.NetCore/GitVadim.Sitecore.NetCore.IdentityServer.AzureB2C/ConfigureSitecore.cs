54382681a528fd19a2feafa6b01ee7dcf3a8080f
Vadim Gusev <vadimnail@mail.ru>
2020-02-26 21:43:48 +0300

    Blank IDS VisualStudio project

---
+++ GitVadim.Examples.NetCore/GitVadim.Sitecore.NetCore.IdentityServer.AzureB2C/ConfigureSitecore.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Sitecore.Framework.Runtime.Configuration;
+
+namespace GitVadim.Sitecore.NetCore.IdentityServer.AzureB2C
+{
+    public class ConfigureSitecore
+    {
+        private readonly ILogger<ConfigureSitecore> _logger;
+
+        public ConfigureSitecore(ISitecoreConfiguration scConfig, ILogger<ConfigureSitecore> logger)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+    }
+}
